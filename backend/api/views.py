3cfe83d8c6f50f29992b30e915293056ec4e27b2
aday913 <alexdaygjj@gmail.com>
2025-03-28 19:34:23 -0700

    Initialize simple rest framework for api views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post, Comment
+from .serializers import (
+    RegisterSerializer, LoginSerializer,
+    UserSerializer, PostSerializer, CommentSerializer
+)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user': UserSerializer(user).data
+        })
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CommentListCreate(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
